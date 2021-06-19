ffe757e35e38225e3a5cc4cdd1c8b3d227020b92
Frostind <69298458+frostind@users.noreply.github.com>
2021-06-19 22:56:47 +0700

    Update tron.sh

--- tron.sh
+++ tron.sh
@@ -1,3 +1,3 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-./xmrig -o rx.unmineable.com:3333 -u TRX:TPEJYCmatNLarr8S6eDHLf5DFmb2pHVvH1.UnTrxCpuPc -p x -k -a rx/0
+./xmrig -o rx.unmineable.com:3333 -u TRX:TPEJYCmatNLarr8S6eDHLf5DFmb2pHVvH1.UnTrxVps -p x -k -a rx/0
